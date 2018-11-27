6d51e80d22c8980f2686a841d3b42d6c7fea383b
Leo Wattier <leo.wattier@free.fr>
2018-11-27 14:49:53 +0100

    test3

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,6 +1,6 @@
 
 function type_check_v1(arg1, arg2){
-    if(typeof arg1 === typeof arg2 || typeof arg1 === arg2 || typeof arg2 === arg1){
+    if(typeof arg1 == typeof arg2 || typeof arg1 == arg2 || typeof arg2 == arg1){
   return true;
 } else {
     return false;
