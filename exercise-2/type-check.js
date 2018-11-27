1aac013cb290324c083b2fe33c49aed1ac6d547a
Leo Wattier <leo.wattier@free.fr>
2018-11-27 15:40:00 +0100

    test5

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,10 +1,17 @@
 
 function type_check_v1(arg1, arg2){
-        if(typeof arg1 == typeof arg2 || typeof arg1 == arg2 || typeof arg2 == arg1){
-    return true;
+    if(typeof arg1 === typeof arg2 || typeof arg1 === arg2 || typeof arg2 === arg1){
+        return true;
+    } else if(arg1 === null && arg2 === null || arg1 === 'null' && arg2 ==='null') {
+        return true;
     } else {
         return false;
     }
 }
 
+function type_check_v2(arg1, arg2){
+
+}
+
   console.log(type_check_v1());
+  
